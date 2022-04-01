c652880f6e655486bed667ddb7f952300bc8eb7a
Paloma-Regis-Ferreira <palomaregis29@gmail.com>
2022-04-01 12:58:19 -0300

    exemplos de mapeamento da requisicao via url

--- src/main/java/com/paloma/carros/api/IndexController.java
+++ src/main/java/com/paloma/carros/api/IndexController.java
@@ -7,22 +7,16 @@ import org.springframework.web.bind.annotation.*;
 public class IndexController {
 
     @GetMapping
-    public String helloget(){
-        return "get spring boot";
+    public String getExemplo1(@RequestParam String nome, @RequestParam String senha){
+        //http://localhost:8080/?nome=paloma&senha=123
+        //primeiro parametro é iniciado com ? e os demais com &
+        return "nome: " + nome + ", senha: " + senha;
     }
 
-    @PostMapping
-    public String hellopost(){
-        return "post spring boot";
+    @GetMapping("/login/{nome}/{senha}")
+    public String getExemplo2(@PathVariable String nome, @PathVariable String senha){
+        //http://localhost:8080/login/paloma/123
+        return "Nome: " + nome +", senha: " + senha;
     }
 
-    @PutMapping
-    public String helloput(){
-        return "put spring boot";
-    }
-
-    @DeleteMapping
-    public String hellodelete(){
-        return "delete spring boot";
-    }
 }
